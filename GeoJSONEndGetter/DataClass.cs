e0d49ada90deeea364eb7ee88121abd4d539cad2
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-29 21:23:16 +0900

    .NET9に　出力データ構造変更　Ichihai1415.GeoJSONを利用　自分用一括出力追加

--- GeoJSONEndGetter/DataClass.cs
+++ GeoJSONEndGetter/DataClass.cs
@@ -1,61 +1,77 @@
-﻿using System.Text.Json.Serialization;
+﻿using Ichihai1415.GeoJSON;
+using System.Text.Json.Serialization;
 
 namespace GeoJSONEndGetter
 {
+    /// <summary>
+    /// データクラス
+    /// </summary>
     public class DataClass
     {
         /// <summary>
-        /// 上端
+        /// 端
         /// </summary>
-        [JsonPropertyName("top")]
-        public double Top { get; set; } = double.NaN;
+        [JsonPropertyName("ends")]
+        public C_Ends? Ends { get; set; } = null;
 
         /// <summary>
-        /// 右端
+        /// 端
         /// </summary>
-        [JsonPropertyName("right")]
-        public double Right { get; set; } = double.NaN;
+        public class C_Ends
+        {
+            /// <summary>
+            /// 上端
+            /// </summary>
+            [JsonPropertyName("top")]
+            public double Top { get; set; } = double.NaN;
 
-        /// <summary>
-        /// 下端
-        /// </summary>
-        [JsonPropertyName("bottom")]
-        public double Bottom { get; set; } = double.NaN;
+            /// <summary>
+            /// 右端
+            /// </summary>
+            [JsonPropertyName("right")]
+            public double Right { get; set; } = double.NaN;
 
-        /// <summary>
-        /// 左端
-        /// </summary>
-        [JsonPropertyName("left")]
-        public double Left { get; set; } = double.NaN;
+            /// <summary>
+            /// 下端
+            /// </summary>
+            [JsonPropertyName("bottom")]
+            public double Bottom { get; set; } = double.NaN;
+
+            /// <summary>
+            /// 左端
+            /// </summary>
+            [JsonPropertyName("left")]
+            public double Left { get; set; } = double.NaN;
+        }
 
         /// <summary>
         /// 各情報
         /// </summary>
         [JsonPropertyName("properties")]
-        public Properties_ Properties { get; set; } = new();
+        public GeoJSONScheme.GeoJSON_JMA_Map.C_Properties_JMA_Map Properties { get; set; } = new();
+    }
+
+    /// <summary>
+    /// GeoJSONの端のデータ(配列にして保存すること)
+    /// </summary>
+    public class GeoJsonEnds
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
 
         /// <summary>
-        /// 各情報
+        /// 更新日(マップデータファイルに記載)
         /// </summary>
-        public class Properties_
-        {
-            /// <summary>
-            /// コード
-            /// </summary>
-            [JsonPropertyName("code")]
-            public int? Code { get; set; } = null;
+        [JsonPropertyName("updateDate")]
+        public required string UpdateDate { get; set; }
 
-            /// <summary>
-            /// 名称
-            /// </summary>
-            [JsonPropertyName("name")]
-            public string Name { get; set; } = string.Empty;
-
-            /// <summary>
-            /// 名称(ひらがな)
-            /// </summary>
-            [JsonPropertyName("namekana")]
-            public string NameKana { get; set; } = string.Empty;
-        }
+        /// <summary>
+        /// 各データ
+        /// </summary>
+        [JsonPropertyName("data")]
+        public required DataClass[] Data { get; set; }
     }
 }
--- GeoJSONEndGetter/Program.cs
+++ GeoJSONEndGetter/Program.cs
@@ -1,122 +1,197 @@
 ﻿using GeoJSONEndGetter;
+using Ichihai1415.GeoJSON;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Unicode;
 
-//コンソール色
-var c_w = Console.ForegroundColor;
-var c_r = ConsoleColor.Red;
-var c_g = ConsoleColor.Green;
-var c_b = ConsoleColor.Blue;
-var c_y = ConsoleColor.Yellow;
-//日本語のまま出力するためのオプション
-//一回しか使わない時CA1869が出る
-#pragma warning disable CA1869 // 'JsonSerializerOptions' インスタンスをキャッシュして再利用する
-var options = new JsonSerializerOptions
+internal class Program
 {
-    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)/*,
-    WriteIndented = true*///インデントするか
-};
-#pragma warning restore CA1869 // 'JsonSerializerOptions' インスタンスをキャッシュして再利用する
-
-restart:
-try
-{
-    //メイン
-    Console.ForegroundColor = c_w;
-    Console.WriteLine("GeoJSONのパスを入力してください。");
-    Console.ForegroundColor = c_b;
-    var path = Console.ReadLine() ?? throw new Exception("パスが入力されていません。");
-    path = path.Replace("\"", "");
-    if (!File.Exists(path)) throw new FileNotFoundException("ファイルが見つかりません。");
-    var rawJson = File.ReadAllText(path) ?? throw new Exception("ファイルの読み込みに失敗しました。");
-    Console.ForegroundColor = c_g;
-    Console.WriteLine("ファイル読み込み完了");
-    var mapJson = JsonNode.Parse(rawJson) ?? throw new Exception("JSONの変換に失敗しました。");
-    Console.WriteLine("JSON一次解析完了");
-    var json_type = mapJson["type"] ?? throw new Exception("JSONの認識に失敗しました。");
-    if (json_type.ToString() != "FeatureCollection") throw new Exception("typeがFeatureCollectionではありません。");
-    var json_features = mapJson["features"] ?? throw new Exception("JSONの処理に失敗しました。", new Exception("featuresが見つかりません。"));
-    //geometry:nullを除く
-    //.Select(f => f!).Where(f => f["geometry"]...を.Where(f => f!["geometry"]にするとJsonNode?[]?となり、後で null可能性警告が出るためこのままで
-    var json_features_valid = json_features.AsArray().Where(f => f != null).Select(f => f!).Where(f => f["geometry"] != null).ToArray();
-    Console.WriteLine("JSON選別完了");
-    Console.WriteLine("各地物処理に移ります...");
-    Console.WriteLine();
-    Console.WriteLine();
-
-    //各地物のデータの抽出
-    //それぞれ緯度経度の配列に変換し最大最小を求めます
-    var endDic = new List<DataClass>();
-    foreach (var feature in json_features_valid)
+    //コンソール色
+    internal static ConsoleColor c_w = Console.ForegroundColor;
+    internal static ConsoleColor c_r = ConsoleColor.Red;
+    internal static ConsoleColor c_g = ConsoleColor.Green;
+    internal static ConsoleColor c_b = ConsoleColor.Blue;
+    internal static ConsoleColor c_y = ConsoleColor.Yellow;
+    internal static readonly JsonSerializerOptions options = new()
     {
-        var name = feature["properties"]!["name"]!.ToString();
-        Console.WriteLine("name=" + name);
-        var geoType = feature["geometry"]!["type"]!.ToString();
-        double[] lats, lons;
-        if (geoType == "Polygon")
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)/*,
+        WriteIndented = true*/
+    };
+
+    private static void Main(string[] args)
+    {
+    //GetAll();
+    //return;
+    restart:
+        try
         {
-            lons = feature["geometry"]!["coordinates"]![0]!.AsArray().Select(x => double.Parse(x![0]!.ToString())).ToArray();
-            lats = feature["geometry"]!["coordinates"]![0]!.AsArray().Select(x => double.Parse(x![1]!.ToString())).ToArray();
-        }
-        else if (geoType == "MultiPolygon")
-        {
-            lons = feature["geometry"]!["coordinates"]!.AsArray().SelectMany((JsonNode? coordinate) => coordinate![0]!.AsArray()).Select(x => double.Parse(x![0]!.ToString())).ToArray();
-            lats = feature["geometry"]!["coordinates"]!.AsArray().SelectMany((JsonNode? coordinate) => coordinate![0]!.AsArray()).Select(x => double.Parse(x![1]!.ToString())).ToArray();
-        }
-        else
-        {
-            Console.ForegroundColor = c_y;
-            Console.WriteLine("警告:typeが正しくありません:" + geoType);
-            continue;
-        }
-        if (lats.Length == 0 || lons.Length == 0)
-            throw new Exception("JSONの処理に失敗しました。", new Exception("緯度経度リストの取得に失敗しました。"));
-        int? code;
-        if (int.TryParse(feature["properties"]!["code"]!.ToString(), out int c))
-            code = c;
-        else
-            code = null;
-        var end = new DataClass
-        {
-            Top = lats.Max(),
-            Right = lons.Max(),
-            Bottom = lats.Min(),
-            Left = lons.Min(),
-            Properties = new DataClass.Properties_
+            //メイン
+            Console.ForegroundColor = c_w;
+            Console.WriteLine("GeoJSONのパスを入力してください。");
+            Console.ForegroundColor = c_b;
+            var path = Console.ReadLine() ?? throw new Exception("パスが入力されていません。");
+            path = path.Replace("\"", "");
+            if (!File.Exists(path)) throw new FileNotFoundException("ファイルが見つかりません。");
+            var rawJson = File.ReadAllText(path) ?? throw new Exception("ファイルの読み込みに失敗しました。");
+            Console.ForegroundColor = c_g;
+            Console.WriteLine("ファイル読み込み完了");
+            var mapJson = GeoJSONHelper.Deserialize<GeoJSONScheme.GeoJSON_JMA_Map>(rawJson) ?? throw new Exception("JSONの変換に失敗しました。");
+            Console.WriteLine("JSON一次解析完了");
+            var json_type = mapJson.Type ?? throw new Exception("JSONの認識に失敗しました。");
+            if (json_type.ToString() != "FeatureCollection") throw new Exception("typeがFeatureCollectionではありません。");
+            var json_features = mapJson.Features ?? throw new Exception("JSONの処理に失敗しました。", new Exception("featuresが見つかりません。"));
+            //geometry:nullを除く
+            //.Select(f => f!).Where(f => f["geometry"]...を.Where(f => f!["geometry"]にするとJsonNode?[]?となり、後で null可能性警告が出るためこのままで
+            //var json_features_valid = json_features.AsArray().Where(f => f != null).Select(f => f!).Where(f => f["geometry"] != null).ToArray();
+            Console.WriteLine("JSON選別完了");
+            Console.WriteLine("各地物処理に移ります...");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //各地物のデータの抽出
+            //それぞれ緯度経度の配列に変換し最大最小を求めます
+            var endDic = new List<DataClass>();
+            foreach (var feature in json_features)
             {
-                Code = code,
-                Name = name,
-                NameKana = feature["properties"]!["namekana"]!.ToString()
+                if (feature.Properties == null) continue;
+                var name = feature.Properties.Name;
+                Console.WriteLine("name=" + name);
+                var geoType = feature.Geometry?.Type;
+                float[] lats, lons;
+                if (feature.Geometry == null)
+                {
+                    var end_null = new DataClass { Properties = feature.Properties };
+                    endDic.Add(end_null);
+                    Console.WriteLine("  no geometry.");
+                    Console.WriteLine();
+                    continue;
+                }
+                else
+                    switch (geoType)
+                    {
+                        case "Polygon":
+                        case "LineString":
+                            lats = [.. feature.Geometry.Coordinates.Objects[0].MainPoints.Select(x => x.Lat)];
+                            lons = [.. feature.Geometry.Coordinates.Objects[0].MainPoints.Select(x => x.Lon)];
+                            break;
+                        case "MultiPolygon":
+                        case "MultiLineString":
+                            lats = [.. feature.Geometry.Coordinates.Objects.SelectMany(c => c.MainPoints.Select(x => x.Lat))];
+                            lons = [.. feature.Geometry.Coordinates.Objects.SelectMany(c => c.MainPoints.Select(x => x.Lon))];
+                            break;
+                        default:
+                            Console.ForegroundColor = c_y;
+                            Console.WriteLine("警告:typeが正しくありません:" + geoType);
+                            Console.WriteLine();
+                            continue;
+
+                    }
+                if (lats.Length == 0 || lons.Length == 0)
+                    throw new Exception("JSONの処理に失敗しました。", new Exception("緯度経度リストの取得に失敗しました。"));
+                var end = new DataClass
+                {
+                    Ends = new DataClass.C_Ends
+                    {
+                        Top = lats.Max(),
+                        Right = lons.Max(),
+                        Bottom = lats.Min(),
+                        Left = lons.Min()
+                    },
+                    Properties = feature.Properties
+                };
+                Console.WriteLine("  top=" + end.Ends.Top + ", right=" + end.Ends.Right + ", bottom=" + end.Ends.Bottom + ", left=" + end.Ends.Left);
+                //Console.WriteLine(JsonSerializer.Serialize(end, options));
+                endDic.Add(end);
+                //throw new Exception();
+                Console.WriteLine();
             }
-        };
-        Console.WriteLine("  top:" + end.Top + ", right=" + end.Right + ", bottom" + end.Bottom + ", left" + end.Left);
-        //Console.WriteLine(JsonSerializer.Serialize(end, options));
-        endDic.Add(end);
-        //throw new Exception();
+            Directory.CreateDirectory("output");
+            var savePath = "output\\" + Path.GetFileName(path).Replace(".geo", ".");
+            File.WriteAllText(savePath, JsonSerializer.Serialize(endDic, options));
+            Console.WriteLine(Path.GetFullPath(savePath) + " に保存しました。");
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = c_r;
+            Console.WriteLine(ex);
+        }
+
+        Console.ForegroundColor = c_w;
         Console.WriteLine();
+        Console.WriteLine("さらに実行する場合Yキーを押してください。他のキーを押すと終了します。");
+        if (Console.ReadKey().Key.ToString() == "Y")
+        {
+            Console.Clear();
+            GC.Collect();
+            goto restart;
+        }
     }
-    Directory.CreateDirectory("output");
-    var savePath = $"output\\{DateTime.Now:yyyyMMddHHmmss}.json";
-    File.WriteAllText(savePath, JsonSerializer.Serialize(endDic, options));
-    Console.WriteLine(Path.GetFullPath(savePath) + " に保存しました。");
 
+    private static void GetAll()
+    {
+        var dict = new List<GeoJsonEnds>();
+        foreach (var path in Directory.GetFiles("C:\\Ichihai1415\\source\\vs\\JmaXmlViewer\\JmaXmlViewer\\bin\\x64\\Debug\\net9.0-windows\\Resources\\MapData", "*_1.geojson"))
+        {
+            Console.WriteLine(Path.GetFileName(path));
+            var rawJson = File.ReadAllText(path);
+            var mapJson = GeoJSONHelper.Deserialize<GeoJSONScheme.GeoJSON_JMA_Map>(rawJson)!;
+            var json_type = mapJson!.Type ?? throw new Exception("JSONの認識に失敗しました。");
+            if (json_type.ToString() != "FeatureCollection") throw new Exception("typeがFeatureCollectionではありません。");
+            var json_features = mapJson.Features ?? throw new Exception("JSONの処理に失敗しました。", new Exception("featuresが見つかりません。"));
+            var endDic = new List<DataClass>();
+            foreach (var feature in json_features)
+            {
+                if (feature.Properties == null) continue;
+                var name = feature.Properties.Name;
+                var geoType = feature.Geometry?.Type;
+                float[] lats, lons;
+                if (feature.Geometry == null)
+                {
+                    var end_null = new DataClass { Properties = feature.Properties };
+                    endDic.Add(end_null);
+                    continue;
+                }
+                else
+                    switch (geoType)
+                    {
+                        case "Polygon":
+                        case "LineString":
+                            lats = [.. feature.Geometry.Coordinates.Objects[0].MainPoints.Select(x => x.Lat)];
+                            lons = [.. feature.Geometry.Coordinates.Objects[0].MainPoints.Select(x => x.Lon)];
+                            break;
+                        case "MultiPolygon":
+                        case "MultiLineString":
+                            lats = [.. feature.Geometry.Coordinates.Objects.SelectMany(c => c.MainPoints.Select(x => x.Lat))];
+                            lons = [.. feature.Geometry.Coordinates.Objects.SelectMany(c => c.MainPoints.Select(x => x.Lon))];
+                            break;
+                        default:
+                            Console.ForegroundColor = c_y;
+                            Console.WriteLine("警告:typeが正しくありません:" + geoType);
+                            continue;
 
-
-}
-catch (Exception ex)
-{
-    Console.ForegroundColor = c_r;
-    Console.WriteLine(ex);
-}
-
-Console.ForegroundColor = c_w;
-Console.WriteLine();
-Console.WriteLine("さらに実行する場合Yキーを押してください。他のキーを押すと終了します。");
-if (Console.ReadKey().Key.ToString() == "Y")
-{
-    Console.Clear();
-    GC.Collect();
-    goto restart;
+                    }
+                if (lats.Length == 0 || lons.Length == 0)
+                    throw new Exception("JSONの処理に失敗しました。", new Exception("緯度経度リストの取得に失敗しました。"));
+                var end = new DataClass
+                {
+                    Ends = new DataClass.C_Ends
+                    {
+                        Top = lats.Max(),
+                        Right = lons.Max(),
+                        Bottom = lats.Min(),
+                        Left = lons.Min()
+                    },
+                    Properties = feature.Properties
+                };
+                //Console.WriteLine(JsonSerializer.Serialize(end, options));
+                endDic.Add(end);
+                //throw new Exception();
+            }
+            var p = Path.GetFileName(path).Split('_');
+            dict.Add(new GeoJsonEnds { Name = Path.GetFileName(path).Replace("_GIS", "*").Split('*')[0], UpdateDate = p[^2], Data = [.. endDic] });
+        }
+        Directory.CreateDirectory("output");
+        File.WriteAllText("output\\GeoJSONEnds.json", JsonSerializer.Serialize(dict, options));
+    }
 }
